308afd3b221dd5cf71829f303db8c6a2a2ab815d
Ana Bulovic <bulovic.ana@gmail.com>
2015-12-29 18:29:19 +0100

    Enabled rectangular zoom

--- flicker_test.cpp
+++ flicker_test.cpp
@@ -89,6 +89,16 @@ int main(int argc, char ** argv)
     h = y1p - y0p; h_ = h;
     float z = 0.95f;
 
+    RectangleShape zoomRect;
+    Vector2i initialPosition = Vector2i(0, 0);
+    zoomRect.setFillColor(Color(76, 165, 0, 100));
+    View zoomView;
+    zoomView.reset(sf::FloatRect(0, 0, 1000, 500));
+
+    //zoomView.setSize(800, 200);
+    zoomView.setViewport(sf::FloatRect(0.f, 0.f, 1.f, 1.f));
+
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -141,12 +151,56 @@ int main(int argc, char ** argv)
                     h_ = h;
                 }
             }
+            if (event.type == Event::MouseButtonPressed && 
+                Keyboard::isKeyPressed(Keyboard::R))
+            {
+                initialPosition = Mouse::getPosition(window);
+                zoomRect.setPosition(initialPosition.x, initialPosition.y);
+                cout << "initial pos: " << initialPosition.x << ", " << initialPosition.y << endl;
+            }
+            if (Keyboard::isKeyPressed(Keyboard::R) && Mouse::isButtonPressed(Mouse::Left))
+            {
+                zoomRect.setPosition(initialPosition.x, initialPosition.y);
+
+                Vector2i newPos = Mouse::getPosition(window);
+                float len = newPos.x - initialPosition.x;
+                float wid = newPos.y - initialPosition.y;
+                zoomRect.setSize(Vector2f(len, wid));
+                cout << "Drawing..." << endl;
+                cout << "X, Y: " << zoomRect.getPosition().x << ", " << zoomRect.getPosition().y << 
+                ", W, H: " << zoomRect.getSize().x << ", " << zoomRect.getSize().y << endl;
+            }
+            if (event.type == Event::MouseButtonReleased && zoomRect.getSize().x > 0)
+            {
+                float x0 = initialPosition.x;
+                float x1 = Mouse::getPosition(window).x;
+                x0 = x0 / window.getSize().x * l_ + x0p_;
+                x1 = x1 / window.getSize().x * l_ + x0p_;
+                float y0 = initialPosition.y;
+                float y1 = Mouse::getPosition(window).y;
+                y0 = y0 / window.getSize().y * h_ + y0p_;
+                y1 = y1 / window.getSize().y * h_ + y0p_;
+
+                upper_view.reset(FloatRect(x0, y0, x1 - x0, y1 - y0));
+
+                x0p_ = x0p = x0;
+                x1p_ = x1p = x1;
+                y0p_ = y0p = y0;
+                y1p_ = y1p = y1;
+                l_ = l = x1 - x0;
+                h_ = h = y1 - y0;
+
+                zoomRect.setSize(Vector2f(0, 0));
+            }
         }
         window.clear(sf::Color::Black);
         //window.draw(line, 2, sf::Lines);
 
         window.setView(upper_view);
         window.draw(tc);
+
+        window.setView(zoomView);
+        window.draw(zoomRect);
 
 
         window.display();
